6c94db23236f04e3e03c5cd484845b74b1021cff
Wael Boutglay <btwael@gmail.com>
2025-12-04 11:15:06 +0100

    [inbox] Test inbox reception by execution count

--- traceforge/examples/inbox_toy_inbox.rs
+++ traceforge/examples/inbox_toy_inbox.rs
@@ -1,75 +1,43 @@
 //extern crate traceforge;
-
 use traceforge::thread;
-use traceforge::thread::ThreadId;
 
 #[derive(Clone, Debug, PartialEq)]
-enum Action {
-    Work,
-    Terminate,
+struct Msg {}
+
+fn simple_inbox(inboxes: u32, senders: u32) -> traceforge::Stats {
+    traceforge::verify(traceforge::Config::builder().build(), move || {
+        let inbx_thread = thread::spawn(move || {
+            for i in 0..inboxes {
+                let _ = traceforge::inbox();
+            }
+        });
+
+        let inb_tid = inbx_thread.thread().id();
+
+        let mut sender_threads = Vec::new();
+
+        for i in 0..senders {
+            let s = thread::spawn(move || {
+                traceforge::send_msg(inb_tid, Msg {});
+            });
+            sender_threads.push(s);
+        }
+
+        for sender_thread in sender_threads {
+            let _ = sender_thread.join();
+        }
+
+        let _ = inbx_thread.join();
+    })
 }
 
-#[derive(Clone, Debug, PartialEq)]
-struct Msg {
-    action: Action,
-    sender: i32,
-}
-
-fn example() {
-    let inbx_thread = thread::spawn(move || {
-        let m = traceforge::inbox();
-    });
-
-    let inb_tid = inbx_thread.thread().id();
-
-    let s1 = thread::spawn(move || {
-        traceforge::send_msg(
-            inb_tid,
-            Msg {
-                action: Action::Work,
-                sender: 0,
-            },
-        );
-    });
-    let s2 = thread::spawn(move || {
-        traceforge::send_msg(
-            inb_tid,
-            Msg {
-                action: Action::Work,
-                sender: 1,
-            },
-        );
-    });
-    let s3 = thread::spawn(move || {
-        traceforge::send_msg(
-            inb_tid,
-            Msg {
-                action: Action::Work,
-                sender: 2,
-            },
-        );
-    });
-
-    let _ = s1.join();
-    let _ = s2.join();
-    let _ = s3.join();
-    let _ = inbx_thread.join();
-}
-
-fn random() {
-    println!("Running the example in random mode");
-    let num = traceforge::test(traceforge::Config::builder().build(), example, 1);
-    println!("Ran {num} tests");
+fn example() -> traceforge::Stats {
+    simple_inbox(4,4)
 }
 
 fn forge() {
     println!("Running the example in systematic mode");
-    let stats = traceforge::verify(
-        traceforge::Config::builder()
-            .with_graph_printing("./tex")
-            .build(),
-        example,
-    );
+    let stats = example();
     println!("Stats = {}, {}", stats.execs, stats.block);
 }
 
@@ -77,16 +45,5 @@ fn main() {
     // Get command line arguments
     let args: Vec<String> = std::env::args().collect();
 
-    // Check if any argument was provided
-    if args.len() < 2 {
-        println!("Usage: {} --random|--forge", args[0]);
-        return;
-    }
-
-    // Match the first argument
-    match args[1].as_str() {
-        "--random" => random(),
-        "--forge" => forge(),
-        _ => println!("Invalid argument. Use --random or --forge"),
-    }
+    forge();
 }
